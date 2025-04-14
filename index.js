164d04944ac1434cd637b2b6737226818926a4fa
Surghe <Milesiii.Irving@gmail.com>
2025-04-13 20:01:55 -0600

    eighth commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import { fetchCharacters } from './api.js';
 import { displayCharacters } from './ui.js';
 
+async function initApp() {
+    console.log('App initialized');
+    const characters = await fetchCharacters();
+    displayCharacters(characters);
+  }
+  
+  initApp();
