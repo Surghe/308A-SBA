9c49c74bb81c2a8fe24270b4ba7b02c0fae12c6c
Surghe <Milesiii.Irving@gmail.com>
2025-04-13 19:59:32 -0600

    seventh commit

--- ui.js
+++ ui.js
@@ -1,4 +1,13 @@
 export function displayCharacters(characters) {
     const container = document.getElementById('character-list');
     container.innerHTML = '';
-}
+    
+    characters.forEach(character => {
+        const charElement = document.createElement('div');
+        charElement.innerHTML = `
+          <h3>${character.name}</h3>
+          <img src="${character.image}" alt="${character.name}" width="150"/>
+        `;
+        container.appendChild(charElement);
+      });
+    }
